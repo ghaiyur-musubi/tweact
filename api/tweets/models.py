1f80c0d509a92cca02be7e055302aba28834f842
Ghaiyur Naqui Ahmad <67789350+ghaiyur-musubi@users.noreply.github.com>
2021-05-21 01:51:55 +0530

    Model Design

--- api/tweets/models.py
+++ api/tweets/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+"""
+user - FK
+body - CharField - max L 255
+created_at - DateTimeField
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+import cloudinary
+
+# Create your models here.
+
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User,
+                             related_name='tweets',
+                             on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=255)
+    image = CloudinaryField(
+        'Image attached to the tweet', blank=True, null=True)
+    like_count = models.PositiveIntegerField(
+        'Like Count', default=0, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Desc Sort the tweets
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return "{0} at {1}: {2}...".format(self.user, self.created_at, self.body[:20])
