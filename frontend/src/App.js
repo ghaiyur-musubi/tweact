872b004c9acd7bacd58db67ae96d5d40c6338055
Ghaiyur Naqui Ahmad <67789350+ghaiyur-musubi@users.noreply.github.com>
2021-06-03 05:18:00 +0530

    App JS placeholder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> T W E A C T </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This Placeholder for Tweact
         </p>
         <a
           className="App-link"
