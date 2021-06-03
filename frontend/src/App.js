5e0337dccb7de0121eb4c9806a9f0e464f3882a0
Ghaiyur Naqui Ahmad <67789350+ghaiyur-musubi@users.noreply.github.com>
2021-06-03 21:57:53 +0530

    Updated for Links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import useState from 'react';
+import axios from 'axios';
 
 function App() {
+  const [tweets ,setTweets] = useState('')
+  axios.get('')
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> T W E A C T </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This Placeholder for Tweact
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> T W E A C T </h1>
+      <ul>
+
+      </ul>
     </div>
   );
 }
