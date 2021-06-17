e24a05930408064ba2a91a62cf6a7723f6914373
Ghaiyur Naqui Ahmad <67789350+ghaiyur-musubi@users.noreply.github.com>
2021-06-17 21:25:37 +0530

    Update operations.js

--- frontend/src/reducks/tweets/operations.js
+++ frontend/src/reducks/tweets/operations.js
@@ -4,3 +4,13 @@ import { push } from "connected-react-router";
 
 const api = new API();
 
+export const fetchTweets = () => {
+    return async (dispatch) => {
+        return api.getTweets()
+            .then((tweets) => {
+                dispatch(fetchTweetsAction(tweets))
+            }).catch((error) => {
+                alert("Failes to connect to API : /tweets/")
+            })
+    }
+}
