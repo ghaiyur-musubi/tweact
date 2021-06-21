352e01009ec2f4b87399293f13306bc2eada7fe7
Ghaiyur Naqui Ahmad <67789350+ghaiyur-musubi@users.noreply.github.com>
2021-06-21 21:34:08 +0530

    Update Tweet.jsx

--- frontend/src/components/Tweets/Tweet.jsx
+++ frontend/src/components/Tweets/Tweet.jsx
@@ -11,3 +11,79 @@ import TimeAgo from "javascript-time-ago";
 import ReactTimeAgo from "react-time-ago";
 import en from "javascript-time-ago/locale/en";
 import API from "../../API";
+
+const api = new API();
+
+TimeAgo.addDefaultLocale(en);
+
+const Tweet = ({ tweet }) => {
+  const dispatch = useDispatch();
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const clickLikeButton = () => {
+    setIsLiked(!isLiked);
+    if (isLiked) {
+      api
+        .likeSubtractTweet(tweet.id)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+      setLikeCount(likeCount - 1);
+    } else {
+      api
+        .likeAddTweet(tweet.id)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
+      setLikeCount(likeCount + 1);
+    }
+  };
+
+  useEffect(() => {
+    setLikeCount(tweet.like_count);
+  }, [tweet]);
+
+  return (
+    <div class="tweet-list">
+      <div class="tweet">
+        <div class="user">
+          <a href="#">
+            <img src={SampleIcon} class="" />
+          </a>
+        </div>
+        <div class="content">
+          <div class="name">
+            <a href="#" class="">
+              <span class="nickname">{tweet.name}</span>
+              <span class="datetime">
+                <ReactTimeAgo date={tweet.updated_at} locale="en-US" timeStyle="twitter" />
+              </span>
+            </a>
+            <img class="menu_icon" onClick={() => setMenuToggle(!menuToggle)} src={IconSmallMenu} />
+            {menuToggle && (
+              <div class="menu">
+                <span onClick={() => dispatch(deleteTweet(tweet.id))}>Delete</span>
+                <br />
+                <span onClick={() => dispatch(push("/edit/" + tweet.id))}>Edit</span>
+              </div>
+            )}
+          </div>
+          <p>{tweet.body}</p>
+          {tweet.image && (
+            <a href={tweet.image} target="_blank">
+              <img class="post-image" src={tweet.image} />
+            </a>
+          )}
+          <div class="icon-list">
+            <div class="like" onClick={clickLikeButton}>
+              {isLiked ? <img src={IconHeartTwitterBlue} /> : <img src={IconHeartGray} />}
+            </div>
+            <div class="like-count">{likeCount}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tweet;
